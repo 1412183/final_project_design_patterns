76e1ea41e0613aec6affee2ce286728e85a39cc1
1412183 <jkp.msd@gmail.com>
2018-06-13 03:34:22 +0700

    create extends class RegexValidatorAttribute

---
+++ SimpleValidationFramework/SimpleValidationLibrary/Attributes/RegexValidatorAttribute.cs
@@ -0,0 +1,30 @@
+﻿using SimpleValidationLibrary.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace SimpleValidationLibrary.Attributes
+{
+    class RegexValidatorAttribute : ValidatorAttribute
+    {
+        private string regex;
+
+        public RegexValidatorAttribute(string regex)
+        {
+            this.regex = regex;
+        }
+
+        public string Regex
+        {
+            get { return this.regex; }
+        }
+
+        public override Validator GetValidator(PropertyInfo propertyInfo)
+        {
+            return new RegexValidator(this.ErrorMessage, propertyInfo, this.regex);
+        }
+    }
+}
