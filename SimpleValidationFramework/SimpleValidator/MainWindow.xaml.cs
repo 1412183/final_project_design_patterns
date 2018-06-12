2646d3024aa892ba3ae7226004b0a1baac5d7bb5
1412183 <jkp.msd@gmail.com>
2018-06-12 20:26:25 +0700

    refactoring

---
+++ SimpleValidationFramework/SimpleValidator/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleValidator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+        
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            Reset();
+
+        }
+
+        public void Reset()
+
+        {
+
+            textBoxFirstName.Text = "";
+
+            textBoxLastName.Text = "";
+
+            textBoxEmail.Text = "";
+
+            textBoxAddress.Text = "";
+
+            passwordBox1.Password = "";
+
+            passwordBoxConfirm.Password = "";
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            Close();
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+
+        {
+
+           
+
+        }
+    }
+}
