8cdd71c4a944db590e90c15acd3a7f0f4a7f58ad
1212186 <m.khoi47@gmail.com>
2018-06-12 22:30:52 +0700

    Create class interface ICustomValidationRule

---
+++ SimpleValidationFramework/SimpleValidation/Interfaces/ICustomValidationRule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleValidation.Interfaces
+{
+    public interface ICustomValidationRule
+    {
+        bool DoIsValid(object value);
+    }
+}
