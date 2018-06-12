f3f881558ac63885e0829df0221f9d37ca1269e7
1412183 <jkp.msd@gmail.com>
2018-06-13 02:06:52 +0700

    create abstract class ValidatorAttribute

---
+++ SimpleValidationFramework/SimpleValidationLibrary/Attributes/ValidatorAttribute.cs
@@ -0,0 +1,36 @@
+﻿using SimpleValidationLibrary.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleValidationLibrary.Attributes
+{
+    public abstract class ValidatorAttribute : Attribute
+    {
+
+        private string errorMessage;
+        private uint priority;
+
+        public ValidatorAttribute()
+        {
+            this.errorMessage = null;
+            this.priority = 0;
+        }
+
+        public string ErrorMessage
+        {
+            get { return this.errorMessage; }
+            set { this.errorMessage = value; }
+        }
+
+        public uint Priority
+        {
+            get { return this.priority; }
+            set { this.priority = value; }
+        }
+        public abstract Validator GetValidator(PropertyInfo propertyInfo);
+    }
+}
