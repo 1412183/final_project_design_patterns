bdea6cd952406d65ce05c318f02a9a7bccb08379
1412183 <jkp.msd@gmail.com>
2018-06-13 02:32:23 +0700

    create class NotifyIssue

---
+++ SimpleValidationFramework/SimpleValidationLibrary/Util/NotifyIssue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleValidationLibrary.Util
+{
+    class NotifyIssue
+    {
+        private List<IObserver> listObservers = null;
+
+        public void Attach(IObserver observer)
+        {
+            if (listObservers == null)
+            {
+                listObservers = new List<IObserver>();
+            }
+            listObservers.Add(observer);
+        }
+
+        protected void Notify()
+        {
+            if (this.listObservers == null) return;
+
+            foreach (IObserver obj in listObservers)
+            {
+                obj.Update(this);
+            }
+        }
+    }
+}
