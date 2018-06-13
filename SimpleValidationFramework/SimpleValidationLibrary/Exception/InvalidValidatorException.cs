9c9228f398d7b4e85797c75e633c2e86a23ee3cf
1412183 <jkp.msd@gmail.com>
2018-06-13 07:06:46 +0700

    update

---
+++ SimpleValidationFramework/SimpleValidationLibrary/Exception/InvalidValidatorException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleValidationLibrary.Exception
+{
+    [Serializable]
+    public class InvalidValidatorException : InvalidOperationException
+    {
+        private Type propertyType;
+        private Type validatorType;
+
+        public InvalidValidatorException(Type validatorType, Type propertyType)
+           : base(string.Format("{0} could not be applied to {1}.", validatorType.FullName, propertyType.FullName))
+        {
+            this.propertyType = propertyType;
+            this.validatorType = validatorType;
+        }
+
+        public Type PropertyType
+        {
+            get { return this.propertyType; }
+        }
+
+        public Type ValidatorType
+        {
+            get { return this.validatorType; }
+        }
+       
+    }
+}
--- SimpleValidationFramework/SimpleValidationLibrary/Validators/Validator.cs
+++ SimpleValidationFramework/SimpleValidationLibrary/Validators/Validator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleValidationLibrary.Exception;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
