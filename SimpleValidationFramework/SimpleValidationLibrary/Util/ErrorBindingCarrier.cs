55c7eca64b40100ba5a78059dc37e3ea8109f4c0
1412183 <jkp.msd@gmail.com>
2018-06-13 07:16:12 +0700

    add abstract class ErrorBindingCarrier

--- SimpleValidationFramework/SimpleValidationLibrary/Util/ErrorBindingCarrier.cs
+++ SimpleValidationFramework/SimpleValidationLibrary/Util/ErrorBindingCarrier.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleValidationLibrary.Primary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace SimpleValidationLibrary.Util
 {
     public abstract class ErrorBindingCarrier : IObserver
     {
+        protected object _target;
+        protected string _propertyName;
+        protected ErrorBindingCarrier(object target, string propertyName)
+        {
+            this._target = target;
+            this._propertyName = propertyName;
+        }
+        public void Update(NotifyIssue subject)
+        {
+            ValidatableBase instance = (ValidatableBase)subject;
+            string errorMessage = "";
+            if (instance.ValidationErrors.ContainsKey(this._propertyName))
+            {
+                errorMessage = instance.ValidationErrors[this._propertyName];
+            }
+            this.SetTargetValue(errorMessage);
+        }
+
+        abstract protected void SetTargetValue(string errorMessage);
     }
 }
