73b6bd15fe26b4059159318b6a8bd9720ddf278f
1412183 <jkp.msd@gmail.com>
2018-06-13 07:33:26 +0700

    update

--- SimpleValidationFramework/SimpleValidationLibrary/Attributes/CompareValidatorAttribute.cs
+++ SimpleValidationFramework/SimpleValidationLibrary/Attributes/CompareValidatorAttribute.cs
@@ -2,7 +2,7 @@
 using SimpleValidationLibrary.Validators;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq;  
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
